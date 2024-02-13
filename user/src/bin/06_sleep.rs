212de324d51a6af696b06e1d075dd5327cca9130
SkyRain <admin@misakacloud.net>
2024-02-13 02:36:07 +0000

    添加测例

---
+++ user/src/bin/06_sleep.rs
@@ -0,0 +1,15 @@
+#![no_std]
+#![no_main]
+
+use ulib::{get_time, println, yield_, TimeVal};
+#[no_mangle]
+fn main() -> i32 {
+    let mut timer = TimeVal::default();
+    get_time(&mut timer);
+    let wait_for = timer.usec + 1000_0;
+    while {get_time(&mut timer); timer.usec} < wait_for {
+        yield_();
+    }
+    println!("Test sleep OK!");
+    0
+}
